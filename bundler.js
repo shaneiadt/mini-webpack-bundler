fa4da454ebf077d19784682c20c6d7e14704ad7a
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-07-28 11:28:00 +0100

    Explicitly include file extentsion type on imports

--- bundler.js
+++ bundler.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const babylon = require('babylon')
 const traverse = require('babel-traverse').default
 
@@ -25,6 +26,28 @@ function createAsset(filename) {
     }
 }
 
-const mainAsset = createAsset('./entry.js')
+function createGraph(entry) {
+    const mainAsset = createAsset(entry)
+    const queue = [mainAsset]
 
-console.log(mainAsset)
+    for (const asset of queue) {
+        const dirname = path.dirname(asset.filename)
+
+        asset.mapping = {}
+        asset.dependencies.forEach(relativePath => {
+            const absolutePath = path.join(dirname, relativePath)
+            const child = createAsset(absolutePath)
+
+            asset.mapping[relativePath] = child.id
+
+            queue.push(child)
+        })
+    }
+
+    return queue
+}
+
+const graph = createGraph('./entry.js')
+
+
+console.log(graph)
