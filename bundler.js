4bf2b6dbfcc1ecb85a0a83fe12ab82ba86d3c141
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-07-28 10:51:24 +0100

    Parse js to an AST object

--- bundler.js
+++ bundler.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
+const babylon = require('babylon')
 
 function createAsset(filename) {
     const content = fs.readFileSync(filename, 'utf-8')
+    const ast = babylon.parse(content, {
+        sourceType: 'module'
+    })
 
-    console.log(content)
+    console.log(ast)
 }
 
 createAsset('./entry.js')
